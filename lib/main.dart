394db14f1272871a194f32e6fa80462b54d26848
Mokin <nikfaut@gmail.com>
2020-12-03 18:15:50 +0500

    Adding detailed comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,64 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart'; //Подключение базового набора виджетов
 
-void main () => runApp(MyApp()); //Создание виджета приложения и запуск класса MyApp
+void main() => //Запуск основной функции приложения main()
+    runApp(MyApp()); //Создание виджета приложения и запуск класса MyApp
 
-class MyApp extends StatelessWidget{ //Класс MyApp не принимающий данные 
+class MyApp extends StatelessWidget { //Класс MyApp не принимающий данные, констаантный виджет
 
-  @override//Переопределение метода build 
-  Widget build(BuildContext content){ //build принимает контекст
-    return new MaterialApp( //возращает MaterialApp
+  @override //С помощью этой аннотации мы говорим, что переопределенный метод из родительского класса мы использовать
+  // не будем?
+  Widget build(BuildContext content) { //build принимает контекст
+    return new MaterialApp(  //возращает MaterialApp
       title: 'MyWords',
-      theme: ThemeData( //Тема на уровне всего приложения 
-        primarySwatch: Colors.orange //Основная палитрца цветов
+      theme: ThemeData( //Тема на уровне всего приложения
+          primarySwatch: Colors.orange //Основная палитрца цветов
+          ),
+      home: Scaffold( //Scaffold набор основныx виджетов
+        appBar: new AppBar( //создание АппБара(В верху)
+          title: Text('MyWords'), //Заголовок виджет текст
         ),
-        home: Scaffold( //Scaffold набор основныx виджетов
-          appBar: new AppBar( //создание АппБара
-            title: Text('MyWords'), //Заголовок виджет текст
-          ),
-          body: Container(
-            child: MyBody (),
-
-              ),
-          ),
-        );
-      }
+        body: Container( //Создание контейнера
+          child: MyBody(), //Дочерний элемент контейнера, class MyBody
+        ),
+      ),
+    );
+  }
 }
 
-class MyBody extends StatefulWidget {
+class MyBody extends StatefulWidget { //Вот эти строки я не понимаю зачем и почему
   @override
-  createState() => new MyListWords ();
+  createState() => new MyListWords();
 }
 
-class MyListWords extends State<MyBody> 
-{
-  List <String> Words = ['Apple','Kiwi', 'Lemon', 'Waterlemon', 'Melon', 'Banana', 'Peach', 'Mandarin', 'Tomate', 'Potate', 'Cucumber', 'Orange'];
-  
+class MyListWords extends State<MyBody> {
+  List<String> Words = [ 
+    'Apple',
+    'Kiwi',
+    'Lemon',
+    'Waterlemon',
+    'Melon',
+    'Banana',
+    'Peach',
+    'Mandarin',
+    'Tomate',
+    'Potate',
+    'Cucumber',
+    'Orange'
+  ]; //Массив, список из моих слов
+
   @override
-  Widget build (BuildContext context) {
-    
-    return ListView.separated( //Виджет возвращает ЛистВью c разделителем между элементами
-      itemCount: Words.length, //Количествоо элементов равно длине массива
-      separatorBuilder: (BuildContext context, int index) => Divider( color: Colors.red),
-      itemBuilder: (context, i){ //??
+  Widget build(BuildContext context) {
+    return ListView.separated( //Виджет возвращает ListView - бесконечный список, c разделителем между элементами
+        itemCount: Words.length, //Количествоо элементов равно длине массива
+        separatorBuilder: (BuildContext context, int index) => Divider(color: Colors.red), //??
+        itemBuilder: (context, i) { //??
 
-      return new ListTile( title: new Text(Words[i], style: TextStyle(height: 2, fontSize: 40, color: Colors.blue))); //ЛистВЬю возвращает ListTile(Что это я хз), с виджетом текст
-
-
-    }); 
+          return new ListTile( // Не совсем понятно, что именно представляет ListTile. Плитка?
+              title: new Text(Words[i], //Вывод элементов массива
+                  style: TextStyle( //Стиль текста
+                      height: 2, //Высота текста
+                      fontSize: 40, //Размер текста
+                      color: Colors.blue))); 
+        });
   }
 }
